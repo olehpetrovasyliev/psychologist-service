8fff978610c37e3ac06aa11eb0650dec823c5e8d
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-11-16 16:03:02 +0200

    add first hero styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,33 @@
+import React from "react";
+import heroImage from "../../assets/images/heroImage.png";
+import styles from "./Home.module.scss";
 const Home = () => {
-  return <section></section>;
+  return (
+    <section className="hero">
+      <div className={styles.heroWrapper}>
+        <div className="content-wrapper">
+          <h1>
+            The road to the <span className="h1-span">depths</span> of the human
+            soul
+          </h1>
+          <p className={styles.heroText}>
+            We help you to reveal your potential, overcome challenges and find a
+            guide in your own life with the help of our experienced
+            psychologists.
+          </p>
+          <button>Get started</button>
+        </div>
+        <div className="image-wrapper">
+          <div className="decor">?</div>
+          <div className="decor">svg</div>
+          <div className="decor">
+            svg <span>Experienced psichologists</span> <span>15,000</span>
+          </div>
+          <img src={heroImage} alt="1" />
+        </div>
+      </div>
+    </section>
+  );
 };
+
+export default Home;
