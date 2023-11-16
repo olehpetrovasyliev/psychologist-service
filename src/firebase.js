f5c2a2cb01ff0a43b419a8382f9786e067c85b3a
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-11-16 13:59:17 +0200

    add firebase data

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA827W-3NjnO77Qlj1Q_76LM6ALRpXV6vk",
+  authDomain: "psychologist-service-b6761.firebaseapp.com",
+  projectId: "psychologist-service-b6761",
+  storageBucket: "psychologist-service-b6761.appspot.com",
+  messagingSenderId: "490016406471",
+  appId: "1:490016406471:web:90e64925ee1ade7740b7e2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
