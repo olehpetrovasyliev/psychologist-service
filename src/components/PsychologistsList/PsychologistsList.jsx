72848259fda6d391ea9d0c71a7f24936a29ad782
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-11-30 01:42:49 +0200

    aff filter

--- src/components/PsychologistsList/PsychologistsList.jsx
+++ src/components/PsychologistsList/PsychologistsList.jsx
@@ -4,12 +4,6 @@ import Select from "react-select";
 import styles from "./PsychologistList.module.scss";
 
 const PsychologistsList = ({ arr }) => {
-  const [visiblePsychologists, setVisiblePsychologists] = useState(3);
-
-  const handleLoadMore = () => {
-    setVisiblePsychologists((prevVisible) => prevVisible + 3);
-  };
-
   const options = [
     { value: "a-z", label: "A to Z" },
     { value: "z-a", label: "Z to A" },
@@ -19,6 +13,37 @@ const PsychologistsList = ({ arr }) => {
     { value: "nonpopular", label: "Not popular" },
     { value: "all", label: "Show all" },
   ];
+  const [visiblePsychologists, setVisiblePsychologists] = useState(3);
+  const [selectedOption, setSelectedOption] = useState(options[6]);
+
+  const handleLoadMore = () => {
+    setVisiblePsychologists((prevVisible) => prevVisible + 3);
+  };
+
+  const handleSelectChange = (selected) => {
+    setSelectedOption(selected);
+  };
+
+  const filteredArr = () => {
+    switch (selectedOption.value) {
+      case "a-z":
+        return arr.slice().sort((a, b) => a.name.localeCompare(b.name));
+      case "z-a":
+        return arr.slice().sort((a, b) => b.name.localeCompare(a.name));
+      case "less10":
+        return arr.filter((el) => el.price_per_hour < 10);
+      case "more10":
+        return arr.filter((el) => el.price_per_hour >= 10);
+      case "popular":
+        return arr.filter((el) => el.rating >= 4.8);
+      case "nonpopular":
+        return arr.filter((el) => el.rating < 4.8);
+
+      default:
+        return arr;
+    }
+  };
+
   const customStyles = {
     control: (provided) => ({
       ...provided,
@@ -53,33 +78,36 @@ const PsychologistsList = ({ arr }) => {
   };
   return (
     <section>
-      <span>Filter</span>
+      <span className={styles.filterLabel}>Filter</span>
       <Select
         options={options}
         styles={customStyles}
         isSearchable={false}
         defaultValue="all"
+        onChange={handleSelectChange}
       />
 
       <ul>
-        {arr.slice(0, visiblePsychologists).map((el) => (
-          <PsychologistCard
-            key={el.id}
-            name={el.name}
-            avatar_url={el.avatar_url}
-            experience={el.experience}
-            price_per_hour={el.price_per_hour}
-            rating={el.rating}
-            license={el.license}
-            specialization={el.specialization}
-            initial_consultation={el.initial_consultation}
-            about={el.about}
-            reviews={el.reviews}
-            id={el.id}
-          />
-        ))}
+        {filteredArr()
+          .slice(0, visiblePsychologists)
+          .map((el) => (
+            <PsychologistCard
+              key={el.id}
+              name={el.name}
+              avatar_url={el.avatar_url}
+              experience={el.experience}
+              price_per_hour={el.price_per_hour}
+              rating={el.rating}
+              license={el.license}
+              specialization={el.specialization}
+              initial_consultation={el.initial_consultation}
+              about={el.about}
+              reviews={el.reviews}
+              id={el.id}
+            />
+          ))}
       </ul>
-      {visiblePsychologists < arr.length && (
+      {visiblePsychologists < filteredArr.length && (
         <button onClick={() => handleLoadMore()} className={styles.loadMore}>
           Load More
         </button>
