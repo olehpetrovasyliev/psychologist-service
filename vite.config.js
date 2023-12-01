73a43dc9cdacc031237e3460ffcac0fdb49d9128
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-12-02 00:58:37 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import reactRouter from "vite-plugin-react-router";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactRouter()],
-  pages: ["/", "/favorites", "/about"],
+  plugins: [react()],
 });
